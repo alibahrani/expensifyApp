a52ffc04a008d67dbdb208eb0d42d6e20737f57f
Ali Al-Bahrani <alibahrani@mac.com>
2019-05-24 09:52:37 -0400

    Learning Array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,30 +1,44 @@
-const person = {
-    name:'Ali',
-    age:26,
-    location: {
-        city: 'Mississauga',
-        temp: 19
-    }
-};
+// //Object Destructuring
 
-const {name = 'Anonymous',age, location} = person;
+// const person = {
+//     name:'Ali',
+//     age:26,
+//     location: {
+//         city: 'Mississauga',
+//         temp: 19
+//     }
+// };
 
-const { city, temp: temprature } = location
+// const {name = 'Anonymous',age, location} = person;
 
-if (city && temprature ) {
-    console.log(`The temp is ${temprature} in ${city}`);
-}
+// const { city, temp: temprature } = location
 
-console.log(`${name} is ${age}.`);
+// if (city && temprature ) {
+//     console.log(`The temp is ${temprature} in ${city}`);
+// }
+
+// console.log(`${name} is ${age}.`);
 
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan holiday',
-    publisher: {
-        // name: 'Penguin'
-    }
-};
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan holiday',
+//     publisher: {
+//         // name: 'Penguin'
+//     }
+// };
 
-const {name: publisherName = 'Self published'} = book.publisher;
-console.log(publisherName);
+// const {name: publisherName = 'Self published'} = book.publisher;
+// console.log(publisherName);
+
+//Array Destructuring
+
+const address = ['6141 quartermain cres', 'Mississauga', 'l5s9v1'];
+
+const [street, city, zipcode] = address;
+console.log(`You are in ${street} ${city}.`)
+
+const item =['Coffee (hot)', '$2.00','$3.00','$4.00']
+
+const [product, small, medium, large] = item;
+console.log(`A medium ${product} costs ${medium}`);
